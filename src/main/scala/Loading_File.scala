cf67b3555bc912b2a9dfa928e9d68873db570d1e
sriragavan.arumugam <sriragavan.arumugam@bankcache.com>
2022-07-14 16:50:12 +0530

    Added Partition By Changes

--- src/main/scala/Loading_File.scala
+++ src/main/scala/Loading_File.scala
@@ -1,12 +1,23 @@
 package com.sr
 
+
 import org.apache.commons.io.filefilter.TrueFileFilter
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.input_file_name
+//import org.apache.log4j.{Logger,Level}
 //import com.typesafe.config.ConfigFactory
 import java.io.File
 
+
+
+
+
 object Loading_File {
   def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
 
     val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
 
@@ -22,14 +33,22 @@ object Loading_File {
       .load("dbfs:/FileStore/shared_uploads/sriragavan.arumugam@aspiresys.com/Financial_year")
 
 
+    val withMetadata = df5.withColumn("inputfilename",input_file_name()).selectExpr("*",s"current_timestamp() as spark_processing_time"
+      ,"year(current_timestamp()) as spark_processing_year"
+      ,"month(current_timestamp()) as spark_processing_month"
+      ,"dayofmonth(current_timestamp()) as spark_processing_day")
+
+
     spark.sql("USE default")
 
     spark.sql("DROP TABLE IF EXISTS Financila_year_prov")
 
-    df5.writeStream.format("delta")
+    withMetadata.writeStream.format("delta")
       .option("checkpointLocation","dbfs:/FileStore/shared_uploads/sriragavan.arumugam@aspiresys.com/Financial_year_checkpoint")
       .option("path", "dbfs:/FileStore/shared_uploads/sriragavan.arumugam@aspiresys.com/Financial_year_outpath")
+      .partitionBy("spark_processing_year","spark_processing_month","spark_processing_day")
       .toTable("Financila_year_prov")
+
 
 
   }
--- src/main/scala/sample_file.scala
+++ src/main/scala/sample_file.scala
@@ -4,11 +4,13 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.Row
 import java.sql.Date
+import org.apache.log4j.{Level, Logger}
 
 object sample_file  {
 
   def main(args: Array[String]): Unit = {
 
+Logger.getLogger("org").setLevel(Level.ERROR)
 
     val spark = SparkSession.builder().master("local").getOrCreate()
 
